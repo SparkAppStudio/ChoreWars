f1b4e38f0295041e2bcbf80e79d527440944dd3f
Douglas Hewitt <doughewitt4@gmail.com>
2017-01-03 22:30:37 -0800

    updated firebase podfile

--- ChoreWars/ChoreWars/ViewController.swift
+++ ChoreWars/ChoreWars/ViewController.swift
@@ -10,21 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var squareView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        squareView = UIView(frame: CGRect(x: self.view.frame.width/2 - 50, y: self.view.frame.height/2 - 50, width: 50, height: 50))
+        view.backgroundColor = UIColor.blue
+        self.view.addSubview(squareView)
+        
+        let button = UIButton(frame: CGRect(x: self.view.frame.width/2 - 100, y: self.view.frame.height/2 - 100, width: 100, height: 30))
+        button.setTitle("Rotate", for: .normal)
+        button.setTitleColor(UIColor.blue, for: .normal)
+        button.addTarget(self, action: #selector(ViewController.didTapButton(sender:)), for: .touchUpInside)
+        self.view.addSubview(button)
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        let nav = UINavigationController()
-        nav.viewControllers.append(RoommatesTableViewController())
-        self.present(nav, animated: true, completion: nil)
+//        let nav = UINavigationController()
+//        nav.viewControllers.append(RoommatesTableViewController())
+//        self.present(nav, animated: true, completion: nil)
+    }
+    
+    func didTapButton(sender: UIButton) {
+        willRotate(degrees: 45)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func willRotate(degrees: CGFloat) {
+//        let context = UIGraphicsGetCurrentContext()
+//        context?.rotate(by: (-degrees * CGFloat.pi) / 180.0)
+        self.squareView.transform = CGAffineTransform(rotationAngle: (-degrees * CGFloat.pi) / 180.0)
+    }
+    
+    func didRotate(degrees: CGFloat) {
+        DispatchQueue.main.async {
+            UIView.animate(withDuration: 0.5, animations: {
+                //                self.cardinalRingImageView.transform = CGAffineTransform(rotationAngle: (-degrees * CGFloat.pi) / 180.0)
+                
+                let context = UIGraphicsGetCurrentContext()
+                context?.rotate(by: (-degrees * CGFloat.pi) / 180.0)
+                
+            })
+        }
     }
 
 
